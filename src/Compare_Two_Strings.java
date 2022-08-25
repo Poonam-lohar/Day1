4b85f4e3f84deffd13f82bc6415c9719c306e4da
Your Name <you@example.com>
2022-08-25 12:18:33 +0530

    added:Day1 program

---
+++ src/Compare_Two_Strings.java
@@ -0,0 +1,10 @@
+public class Compare_Two_Strings {
+    public static void main(String[] args) {
+        String a = "Welcome to java";
+        String b = "Welcome to World";
+        String c = "Welcome to java";
+        System.out.println(a.equals(b));
+        System.out.println(b.equals(c));
+        System.out.println(a.equals(c));
+    }
+}
